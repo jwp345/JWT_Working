29ee2ea93d7a10861eae048402d67ae9f3fc155e
jwp345 <jwp135@naver.com>
2022-08-26 23:08:41 +0900

    oAuth 문제 해결

--- src/main/java/com/likelion/stepstone/authentication/PrincipalOauth2UserService.java
+++ src/main/java/com/likelion/stepstone/authentication/PrincipalOauth2UserService.java
@@ -1,17 +1,12 @@
 package com.likelion.stepstone.authentication;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.likelion.stepstone.authentication.jwt.JwtProperties;
+
 import com.likelion.stepstone.authentication.jwt.JwtTokenProvider;
 import com.likelion.stepstone.authentication.provider.GoogleUserInfo;
 import com.likelion.stepstone.authentication.provider.NaverUserInfo;
 import com.likelion.stepstone.authentication.provider.OAuth2UserInfo;
 import com.likelion.stepstone.user.UserRepository;
 import com.likelion.stepstone.user.model.UserEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -20,7 +15,6 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
 import java.util.Map;
 
 @Service
@@ -52,26 +46,21 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
       oAuth2UserInfo = new NaverUserInfo((Map)oAuth2User.getAttributes().get("response"));
     } else {
       System.out.println("현재 구글과 네이버만 지원 중입니다.");
+      return null;
     }
 
     String userId = oAuth2UserInfo.getUserId();
     String password = bCryptPasswordEncoder.encode("StepStone"); // 의미X
     String role = "ROLE_USER";
+    String name = oAuth2UserInfo.getName();
 
-    UserEntity userEntity = userRepository.findByUserId(userId).orElseThrow(() -> new UsernameNotFoundException("Invalid userId"));
-
-    if(userEntity == null) {
-      System.out.println("최초 로그인");
-      userEntity = UserEntity.builder()
-              .userId(userId)
-              .password(password)
-              .name(oAuth2UserInfo.getName())
-              .roles(role)
-              .build();
-      userRepository.save(userEntity);
-    } else {
-      System.out.println("로그인 한 적이 있습니다.");
-    }
+    UserEntity userEntity = userRepository.findByUserId(userId).orElseGet(() ->
+            userRepository.save(UserEntity.builder()
+            .userId(userId)
+            .password(password)
+            .name(name)
+            .roles(role)
+            .build()));
 
     PrincipalDetails principalDetails = new PrincipalDetails(userEntity);
 
